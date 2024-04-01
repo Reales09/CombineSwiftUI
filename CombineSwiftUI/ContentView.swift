ed29a93fe645a0f850f08f4d49f40a1d252c462b
Reales Myles <rclarence123@gmail.com>
2024-04-01 02:19:40 -0500

    Uso en multiples vistas

--- CombineSwiftUI/ContentView.swift
+++ CombineSwiftUI/ContentView.swift
@@ -10,11 +10,36 @@ import SwiftUI
 struct ContentView: View {
     
     @ObservedObject var contador = Modelo()
+    @State private var titulo = ""
+    
+    
+    var body: some View {
+        VStack {
+            Text(titulo).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            Text("Contador = \(contador.contador)")
+
+            Button("Sumar Contador"){
+                //
+                contador.contador += 1
+            }
+            Button("Titulo"){
+                //
+                titulo = "Nuevo titulo"
+            }
+            Vista2()
+        }
+        .padding()
+    }
+}
+
+struct Vista2: View {
+    
+    @ObservedObject var contador = Modelo()
     
     var body: some View {
         VStack {
             
-            Text("Contador = \(contador.contador)")
+            Text("Contador de vista 2(ObservedObject) = \(contador.contador)")
 
             Button("Sumar Contador"){
                 //
